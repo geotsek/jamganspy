741092940400f7990fa0c574525f9919a149218a
georgios <geotsek@gmail.com>
2020-07-29 19:08:07 +0200

    made random sample of input data options, prints numpy array in to outfile, plot

--- feedforwardGANs.py
+++ feedforwardGANs.py
@@ -7,6 +7,11 @@ Created on Mon Jul 27 16:40:28 2020
 """
 
 import sys
+import os
+import numpy as np
+
+import matplotlib
+import matplotlib.pyplot as plt
 
 import torch
 from torch import nn, optim
@@ -19,13 +24,48 @@ import classesGANs
 from utils import Logger
 
 
+baseDir = '/Users/georgio/MyStuff/MyData/jamGANsOut'
+file_data_out = baseDir + os.sep + 'jamGANsOut.dat'
+
+#allORsomeORlabeled = 0 (all), 1 (some), 2(labels) 
+allORsomeORlabeled = 0
+
+# Number of steps to apply to the discriminator relative to number of steps of generator (GT: i think)
+d_steps = 1  # In Goodfellow et. al 2014 this variable is assigned to 1
+
+# Number of epochs
+#num_epochs = 200
+num_epochs = 10
+
 ##LOAD DATA
 ## data come from http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz  || 60k images of hand-written digits
 DATA_FOLDER = '/Users/georgio/MyStuff/MyData/MNIST'
 # Load data
 data = functionsGANs.mnist_data(DATA_FOLDER)
-# Create loader with data, so that we can iterate over it
-data_loader = torch.utils.data.DataLoader(data, batch_size=100, shuffle=True)
+batch_size = 100
+
+
+if allORsomeORlabeled == 0:
+    # Create loader with data, so that we can iterate over it
+    data1 = data
+    data_loader = torch.utils.data.DataLoader(data1, batch_size, shuffle=True)
+
+if allORsomeORlabeled == 1:
+    # Create loader with data split to given size
+    dataset_total = 60000
+    dataset_use = 1000
+    dataset = torch.utils.data.dataset.random_split(data,(dataset_use,dataset_total-dataset_use))
+    data1 = dataset[0]
+    data_loader = torch.utils.data.DataLoader(data1, batch_size, shuffle=True)
+
+if allORsomeORlabeled == 2:
+    # Create loader with data label selection. GT: DOES NOT WORK WITH LOADER like this 
+    indices = np.where( (data.targets==0) + (data.targets==1) )
+    dataset = torch.utils.data.Subset(data, indices)
+    #dataset =  torch.utils.data.SubsetRandomSampler(indices)
+    data1 = dataset
+    data_loader = torch.utils.data.DataLoader(data1, batch_size, shuffle=True)
+
 # Num batches
 num_batches = len(data_loader)
 
@@ -82,17 +122,12 @@ def train_generator( optimizer, fake_data):
 
 
 
-# Number of steps to apply to the discriminator relative to number of steps of generator (GT: i think)
-d_steps = 1  # In Goodfellow et. al 2014 this variable is assigned to 1
 
-# Number of epochs
-num_epochs = 200
-#num_epochs = 1
 
 num_test_samples = 16
 test_noise = functionsGANs.noise(num_test_samples)
 
-
+all_data_out = []
 
 
 # Create logger instance
@@ -101,6 +136,7 @@ logger = Logger(model_name='ffGAN', data_name='MNIST')
 # num_epochs = 200
 for epoch in range(num_epochs):
     for n_batch, (real_batch,_) in enumerate(data_loader):
+                
         N = real_batch.size(0)
         # 1. Train Discriminator
         real_data = Variable(functionsGANs.images_to_vectors(real_batch))
@@ -124,10 +160,26 @@ for epoch in range(num_epochs):
             logger.log_images(test_images, num_test_samples, epoch, n_batch, num_batches)
             # Display status Logs
             logger.display_status(epoch, num_epochs, n_batch, num_batches, d_error, g_error, d_pred_real, d_pred_fake)
+            #input('press any kay to cont...')
+
+        #store in numpy array output data
+        some_data_out = []
+        step = epoch * num_batches + n_batch
+        some_data_out = np.array([step, d_error.item(),g_error.item(),d_pred_real.mean().item(),d_pred_fake.mean().item()])
+        some_data_out = np.expand_dims(some_data_out, axis=0)
+            
+        if epoch == 0 and n_batch == 0:
+            #print("A..."," epoch = ", epoch," n_batch = ", n_batch)
+            all_data_out = np.copy(some_data_out)
+                
+        else:
+            #print("B..."," epoch = ", epoch," n_batch = ", n_batch)
+            all_data_out = np.vstack((all_data_out, some_data_out))
             
             
             
-            
+print('np.shape(all_data_out)=',np.shape(all_data_out))
+np.savetxt(file_data_out, all_data_out, delimiter='\t',fmt='%.16f')
 
 
 
---
+++ plot.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 19 2019
+
+@author: georgios
+"""
+
+import os
+import sys
+
+import numpy as np
+
+import matplotlib
+import matplotlib.pyplot as plt
+
+from matplotlib import cm
+from matplotlib import rc
+from matplotlib import rcParams
+
+
+#import matplotlib.style
+#matplotlib.style.use('classic')
+
+topDir = '/Users/georgio/MyStuff/MyData/jamGANsOut'
+    
+#print(baseDir)
+
+outFig1pdf = topDir + os.sep + 'jamGANsOut' + '_.pdf'
+outFig1png = topDir + os.sep + 'jamGANsOut' + '_.png'
+
+lbls = np.array(['step','d_error','g_error','d_pred_real','d_pred_fake'])
+
+#cmap = cm.get_cmap("rainbow")
+#cmap = cm.get_cmap("prism")
+clr=cm.rainbow(np.linspace(0,1,len(lbls)))
+clr1=cm.rainbow(np.linspace(0,1,len(lbls)))
+
+markerLineStyle=('x-','s-','o-','^-','+-','v-')
+markerStyle=('x','s','o','^','+','v')
+
+font = {'size' :15}
+matplotlib.rc('font', **font)
+plt.figure()
+rcParams.update({'figure.autolayout': True})
+
+fig, axs = plt.subplots( 2, 2, sharex=True, gridspec_kw={'hspace': 0.5,'wspace': 0.5}, figsize=(6,5))
+
+fileFs = topDir + os.sep + 'jamGANsOut.dat'
+
+S=[]
+S = np.loadtxt(fileFs,dtype=np.float64)
+
+axs[0,0].plot(S[:,0],S[:,1], markerLineStyle[0], color=clr[0], markeredgewidth=0.0, markersize=2, fillstyle='none', label=('s='+lbls[1]))
+axs[0,1].plot(S[:,0],S[:,2], markerLineStyle[0], color=clr[1], markeredgewidth=0.0, markersize=2, fillstyle='none', label=('s='+lbls[2]))
+axs[1,0].plot(S[:,0],S[:,2], markerLineStyle[0], color=clr[2], markeredgewidth=0.0, markersize=2, fillstyle='none', label=('s='+lbls[3]))
+axs[1,1].plot(S[:,0],S[:,3], markerLineStyle[0], color=clr[3], markeredgewidth=0.0, markersize=2, fillstyle='none', label=('s='+lbls[4]))
+
+
+
+axs[0,0].set_xlabel('step',size=12)
+axs[0,0].set_ylabel(lbls[1],size=12)
+axs[0,1].set_xlabel('step',size=12)
+axs[0,1].set_ylabel(lbls[2],size=12)
+axs[1,0].set_xlabel('step',size=12)
+axs[1,0].set_ylabel(lbls[3],size=12)
+axs[1,1].set_xlabel('step',size=12)
+axs[1,1].set_ylabel(lbls[4],size=12)
+
+
+#fig.text(0.5, 0.015, r'$h$', ha='center', rotation='horizontal')
+#fig.text(0.0, 0.52, r'$\mathrm{D}(h)$', va='center', rotation='vertical')
+#fig.text(0.82, 0.89, 'HEX', va='center', rotation='horizontal',fontsize=16)
+#fig.text(0.82, 0.48, 'FCC', va='center', rotation='horizontal',fontsize=16)
+
+#plt.xlim([1e-9,1e0])
+#plt.xticks([1e-8,1e-6,1e-4,1e-2,1e0])
+#plt.ylim([2e-5,6e+4])
+#plt.yticks([1e-4,1e-2,1e+0,1e+2,1e+4])
+
+#plt.legend(loc = 'lower left',prop={'size': 11})
+#axs[0].legend(loc = 'lower left',prop={'size': 8})
+#axs[1].legend(loc = 'lower left',prop={'size': 8})
+    
+#title = crystalFlag + ',Gaps3,N=' + str(nParticles) + ',Ns=' + str(numFiles)
+#fig.suptitle(title,fontsize=20)
+#fig.savefig(outFig1pdf)
+#fig.savefig(outFig1png,dpi=400)
+
+print(" - - - - - The End! - - - - - ")
+
+
+
+
+
